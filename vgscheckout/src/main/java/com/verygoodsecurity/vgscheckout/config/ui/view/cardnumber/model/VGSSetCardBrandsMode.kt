3b510f24fbfcbda399a330058fa6dcc7b05c58a8
dmytrodmytryshyn <dmytro.dmytryshyn@vgs.io>
2021-06-16 17:05:35 +0300

    Add comment

--- vgscheckout/src/main/java/com/verygoodsecurity/vgscheckout/config/ui/view/cardnumber/model/VGSSetCardBrandsMode.kt
+++ vgscheckout/src/main/java/com/verygoodsecurity/vgscheckout/config/ui/view/cardnumber/model/VGSSetCardBrandsMode.kt
@@ -2,6 +2,9 @@ package com.verygoodsecurity.vgscheckout.config.ui.view.cardnumber.model
 
 enum class VGSSetCardBrandsMode {
 
+    /** Modify current card brands or/and add custom. */
     MODIFY,
+
+    /** Replace current card brands with modified or/custom card brands. Only replaced brands will be detected as valid brands. */
     REPLACE
 }
