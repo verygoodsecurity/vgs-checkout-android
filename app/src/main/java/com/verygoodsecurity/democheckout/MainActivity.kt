6560b685e4815b11a3dd259d414e13a90e148cb2
dmytrodmytryshyn <dmytro.dmytryshyn@vgs.io>
2021-07-21 14:59:28 +0300

    Improve demo app

--- app/src/main/java/com/verygoodsecurity/democheckout/MainActivity.kt
+++ app/src/main/java/com/verygoodsecurity/democheckout/MainActivity.kt
@@ -3,17 +3,14 @@ package com.verygoodsecurity.democheckout
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.button.MaterialButton
 import com.verygoodsecurity.democheckout.util.extension.showShort
 import com.verygoodsecurity.vgscheckout.CHECKOUT_RESULT_EXTRA_KEY
 import com.verygoodsecurity.vgscheckout.VGSCheckout
 import com.verygoodsecurity.vgscheckout.config.VGSCheckoutConfiguration
-import com.verygoodsecurity.vgscheckout.config.VGSCheckoutMultiplexingConfiguration
 import com.verygoodsecurity.vgscheckout.config.networking.VGSCheckoutRouteConfiguration
 import com.verygoodsecurity.vgscheckout.config.ui.VGSCheckoutFormConfiguration
-import com.verygoodsecurity.vgscheckout.config.ui.VGSCheckoutMultiplexingFormConfiguration
 import com.verygoodsecurity.vgscheckout.config.ui.view.address.VGSCheckoutAddressOptions
 import com.verygoodsecurity.vgscheckout.config.ui.view.address.address.VGSCheckoutCityAddressOptions
 import com.verygoodsecurity.vgscheckout.config.ui.view.address.city.VGSCheckoutCityOptions
@@ -27,13 +24,14 @@ import com.verygoodsecurity.vgscheckout.config.ui.view.card.cvc.VGSCheckoutCVCOp
 import com.verygoodsecurity.vgscheckout.config.ui.view.card.expiration.VGSCheckoutExpirationDateOptions
 import com.verygoodsecurity.vgscheckout.model.VGSCheckoutResult
 
-class MainActivity : AppCompatActivity(), View.OnClickListener {
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        findViewById<MaterialButton>(R.id.mbBasicFlow).setOnClickListener(this)
-        findViewById<MaterialButton>(R.id.mbMultiplexingFlow).setOnClickListener(this)
+        findViewById<MaterialButton>(R.id.mbPay).setOnClickListener {
+            VGSCheckout().present(this, 1, getCheckoutConfig())
+        }
     }
 
     @Suppress("DEPRECATION")
@@ -42,16 +40,9 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         when (resultCode) {
             Activity.RESULT_OK -> {
                 val result = data?.getParcelableExtra<VGSCheckoutResult>(CHECKOUT_RESULT_EXTRA_KEY)
-                showShort("Checkout complete: code = ${result?.code}, message = ${result?.body}")
+                showShort("Checkout complete (code = ${result?.code})")
             }
             Activity.RESULT_CANCELED -> showShort("Checkout canceled")
-        }
-    }
-
-    override fun onClick(v: View?) {
-        when (v?.id) {
-            R.id.mbBasicFlow -> VGSCheckout().present(this, 1, getCheckoutConfig())
-            R.id.mbMultiplexingFlow -> VGSCheckout().present(this, 1, getMultiplexingConfig())
         }
     }
 
@@ -66,66 +57,56 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         .build()
 
     private fun getCheckoutFormConfig() = VGSCheckoutFormConfiguration.Builder()
-        .setPayButtonTitle("10$")
-        .setCardOptions(
-            VGSCheckoutCardOptions.Builder()
-                .setCardHolderOptions(getCardHolderOptions())
-                .setCardNumberOptions(getCardNumberOptions())
-                .setExpirationDateOptions(getExpirationDateOptions())
-                .setCVCOptions(getCVCOptions())
+        .setPayButtonTitle("9.73$")
+        .setCardOptions(getCardOptions())
+        .setAddressOptions(getAddressOptions())
+        .build()
+
+    private fun getCardOptions() = VGSCheckoutCardOptions.Builder()
+        .setCardHolderOptions(
+            VGSCheckoutCardHolderOptions.Builder()
+                .setFieldName("card_data.card_holder")
                 .build()
+        )
+        .setCardNumberOptions(
+            VGSCheckoutCardNumberOptions.Builder()
+                .setFieldName("card_data.card_number")
+                .build()
+        )
+        .setExpirationDateOptions(
+            VGSCheckoutExpirationDateOptions.Builder()
+                .setFieldName("card_data.exp_date")
+                .build()
+        )
+        .setCVCOptions(
+            VGSCheckoutCVCOptions.Builder()
+                .setFieldName("card_data.card_cvc")
+                .build()
+        )
+        .build()
+
+    private fun getAddressOptions() = VGSCheckoutAddressOptions.Builder()
+        .setCountryOptions(
+            VGSCheckoutCountryOptions.Builder()
+                .setFieldName("address.country")
+                .build()
+        )
+        .setRegionOptions(
+            VGSCheckoutRegionOptions.Builder().setFieldName("address_info.region").build()
+        )
+        .setCityOptions(
+            VGSCheckoutCityOptions.Builder().setFieldName("address_info.city").build()
         )
         .setAddressOptions(
-            VGSCheckoutAddressOptions.Builder()
-                .setCountryOptions(
-                    VGSCheckoutCountryOptions.Builder()
-                        .setFieldName("address.country")
-                        .build()
-                )
-                .setRegionOptions(
-                    VGSCheckoutRegionOptions.Builder().setFieldName("address.region").build()
-                )
-                .setCityOptions(
-                    VGSCheckoutCityOptions.Builder().setFieldName("address.city").build()
-                )
-                .setAddressOptions(
-                    VGSCheckoutCityAddressOptions.Builder()
-                        .setFieldName("address.city_address")
-                        .build()
-                )
-                .setPostalAddressOptions(
-                    VGSCheckoutPostalAddressOptions.Builder()
-                        .setFieldName("address.postal_address")
-                        .build()
-                )
+            VGSCheckoutCityAddressOptions.Builder()
+                .setFieldName("address_info.address")
+                .build()
+        )
+        .setPostalAddressOptions(
+            VGSCheckoutPostalAddressOptions.Builder()
+                .setFieldName("address_info.postal_address")
                 .build()
         )
         .build()
-
-    private fun getCardHolderOptions() = VGSCheckoutCardHolderOptions.Builder()
-        .setFieldName("card_data.personal_data.cardHolder")
-        .build()
-
-    private fun getCardNumberOptions() = VGSCheckoutCardNumberOptions.Builder()
-        .setFieldName("cardNumber")
-        .build()
-
-    private fun getExpirationDateOptions() = VGSCheckoutExpirationDateOptions.Builder()
-        .setFieldName("ard_data.personal_data.secret.expDate")
-        .build()
-
-    private fun getCVCOptions() = VGSCheckoutCVCOptions.Builder()
-        .setFieldName("card_data.cardCvc")
-        .build()
-    //endregion
-
-    //region Checkout multiplexing config
-    private fun getMultiplexingConfig() =
-        VGSCheckoutMultiplexingConfiguration.Builder("tntpszqgikn")
-            .setFormConfig(
-                VGSCheckoutMultiplexingFormConfiguration.Builder()
-                    .setPayButtonTitle("11.99$").build()
-            )
-            .build()
     //endregion
 }
