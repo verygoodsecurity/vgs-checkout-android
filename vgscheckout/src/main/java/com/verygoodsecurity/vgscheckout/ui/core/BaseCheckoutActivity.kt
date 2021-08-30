daa8542ef6decb5f192bdbdfacce237cb600a4b9
dmytrodmytryshyn <dmytro.dmytryshyn@vgs.io>
2021-08-30 16:27:07 +0300

    Add card details validation

--- vgscheckout/src/main/java/com/verygoodsecurity/vgscheckout/ui/core/BaseCheckoutActivity.kt
+++ vgscheckout/src/main/java/com/verygoodsecurity/vgscheckout/ui/core/BaseCheckoutActivity.kt
@@ -4,6 +4,7 @@ import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import androidx.annotation.CallSuper
+import androidx.annotation.StringRes
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.button.MaterialButton
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
@@ -11,10 +12,8 @@ import com.verygoodsecurity.vgscheckout.R
 import com.verygoodsecurity.vgscheckout.collect.core.VGSCollect
 import com.verygoodsecurity.vgscheckout.collect.core.VgsCollectResponseListener
 import com.verygoodsecurity.vgscheckout.collect.core.model.network.VGSResponse
-import com.verygoodsecurity.vgscheckout.collect.widget.CardVerificationCodeEditText
-import com.verygoodsecurity.vgscheckout.collect.widget.ExpirationDateEditText
-import com.verygoodsecurity.vgscheckout.collect.widget.PersonNameEditText
-import com.verygoodsecurity.vgscheckout.collect.widget.VGSCardNumberEditText
+import com.verygoodsecurity.vgscheckout.collect.view.InputFieldView
+import com.verygoodsecurity.vgscheckout.collect.widget.*
 import com.verygoodsecurity.vgscheckout.config.core.CheckoutConfiguration
 import com.verygoodsecurity.vgscheckout.config.ui.view.card.cardholder.VGSCheckoutCardHolderOptions
 import com.verygoodsecurity.vgscheckout.config.ui.view.card.cardnumber.VGSCheckoutCardNumberOptions
@@ -24,7 +23,7 @@ import com.verygoodsecurity.vgscheckout.model.CheckoutResultContract
 import com.verygoodsecurity.vgscheckout.util.extension.*
 
 internal abstract class BaseCheckoutActivity<C : CheckoutConfiguration> :
-    AppCompatActivity(), VgsCollectResponseListener {
+    AppCompatActivity(), VgsCollectResponseListener, InputFieldView.OnTextChangedListener {
 
     protected val config: C by lazy { resolveConfig(intent) }
 
@@ -34,9 +33,13 @@ internal abstract class BaseCheckoutActivity<C : CheckoutConfiguration> :
         }
     }
 
+    private lateinit var cardHolderTil: VGSTextInputLayout
     private lateinit var cardHolderTied: PersonNameEditText
+    private lateinit var cardNumberTil: VGSTextInputLayout
     private lateinit var cardNumberTied: VGSCardNumberEditText
+    private lateinit var expirationDateTil: VGSTextInputLayout
     private lateinit var expirationDateTied: ExpirationDateEditText
+    private lateinit var securityCodeTil: VGSTextInputLayout
     private lateinit var securityCodeTied: CardVerificationCodeEditText
 
     private lateinit var saveCardButton: MaterialButton
@@ -72,6 +75,15 @@ internal abstract class BaseCheckoutActivity<C : CheckoutConfiguration> :
         finish()
     }
 
+    override fun onTextChange(view: InputFieldView, isEmpty: Boolean) {
+        when (view.id) {
+            R.id.vgsTiedCardHolder -> cardHolderTil.setError(null)
+            R.id.vgsTiedCardNumber -> cardNumberTil.setError(null)
+            R.id.vgsTiedExpirationDate -> expirationDateTil.setError(null)
+            R.id.vgsTiedSecurityCode -> securityCodeTil.setError(null)
+        }
+    }
+
     @CallSuper
     protected open fun initView(savedInstanceState: Bundle?) {
         initToolbar()
@@ -96,29 +108,37 @@ internal abstract class BaseCheckoutActivity<C : CheckoutConfiguration> :
     }
 
     private fun initCardHolderView(options: VGSCheckoutCardHolderOptions) {
+        cardHolderTil = findViewById(R.id.vgsTilCardHolder)
         cardHolderTied = findViewById(R.id.vgsTiedCardHolder)
         cardHolderTied.setFieldName(options.fieldName)
+        cardHolderTied.addOnTextChangeListener(this)
         collect.bindView(cardHolderTied)
     }
 
     private fun initCardNumberView(options: VGSCheckoutCardNumberOptions) {
+        cardNumberTil = findViewById(R.id.vgsTilCardNumber)
         cardNumberTied = findViewById(R.id.vgsTiedCardNumber)
         cardNumberTied.setFieldName(options.fieldName)
         cardNumberTied.setValidCardBrands(options.cardBrands)
         cardNumberTied.setIsCardBrandPreviewHidden(options.isIconHidden)
+        cardNumberTied.addOnTextChangeListener(this)
         collect.bindView(cardNumberTied)
     }
 
     private fun initExpirationDateView(options: VGSCheckoutExpirationDateOptions) {
+        expirationDateTil = findViewById(R.id.vgsTilExpirationDate)
         expirationDateTied = findViewById(R.id.vgsTiedExpirationDate)
         expirationDateTied.setFieldName(options.fieldName)
+        expirationDateTied.addOnTextChangeListener(this)
         collect.bindView(expirationDateTied)
     }
 
     private fun initSecurityCodeView(options: VGSCheckoutCVCOptions) {
+        securityCodeTil = findViewById(R.id.vgsTilSecurityCode)
         securityCodeTied = findViewById(R.id.vgsTiedSecurityCode)
         securityCodeTied.setFieldName(options.fieldName)
         securityCodeTied.setIsPreviewIconHidden(options.isIconHidden)
+        securityCodeTied.addOnTextChangeListener(this)
         collect.bindView(securityCodeTied)
     }
 
@@ -130,7 +150,53 @@ internal abstract class BaseCheckoutActivity<C : CheckoutConfiguration> :
     }
 
     private fun validate(): Boolean {
-        return true
+
+        fun validate(
+            target: InputFieldView,
+            parent: VGSTextInputLayout,
+            @StringRes emptyError: Int,
+            @StringRes invalidError: Int
+        ): Boolean = when {
+            target.getFieldState()?.isEmpty == true -> {
+                parent.setError(emptyError)
+                false
+            }
+            target.getFieldState()?.isValid == false -> {
+                parent.setError(invalidError)
+                false
+            }
+            else -> true
+        }
+
+        val isCardHolderValid = validate(
+            cardHolderTied,
+            cardHolderTil,
+            R.string.vgs_checkout_card_holder_empty_error,
+            0
+        )
+
+        val isCardNumberValid = validate(
+            cardNumberTied,
+            cardNumberTil,
+            R.string.vgs_checkout_card_number_empty_error,
+            R.string.vgs_checkout_card_number_invalid_error
+        )
+
+        val isExpirationDateValid = validate(
+            expirationDateTied,
+            expirationDateTil,
+            R.string.vgs_checkout_card_expiration_date_empty_error,
+            R.string.vgs_checkout_card_expiration_date_invalid_error
+        )
+
+        val isSecurityCodeValid = validate(
+            securityCodeTied,
+            securityCodeTil,
+            R.string.vgs_checkout_card_verification_code_empty_error,
+            R.string.vgs_checkout_card_verification_code_invalid_error
+        )
+
+        return isCardHolderValid && isCardNumberValid && isExpirationDateValid && isSecurityCodeValid
     }
 
     private fun showConfirmDialog(onConfirmed: () -> Unit) {
