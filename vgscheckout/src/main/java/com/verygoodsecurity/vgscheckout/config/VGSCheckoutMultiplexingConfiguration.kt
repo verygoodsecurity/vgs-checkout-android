7a08911ef0095eafb4375b0a1575e2659e6a4ecc
dmytrodmytryshyn <dmytro.dmytryshyn@vgs.io>
2021-09-03 14:50:02 +0300

    Add token parameter to VGSCheckoutMultiplexingConfiguration.kt

--- vgscheckout/src/main/java/com/verygoodsecurity/vgscheckout/config/VGSCheckoutMultiplexingConfiguration.kt
+++ vgscheckout/src/main/java/com/verygoodsecurity/vgscheckout/config/VGSCheckoutMultiplexingConfiguration.kt
@@ -8,17 +8,24 @@ import kotlinx.parcelize.Parcelize
 
 @Parcelize
 class VGSCheckoutMultiplexingConfiguration private constructor(
+    internal val token: String,
     override val vaultID: String,
     override val environment: String,
     override val routeConfig: VGSCheckoutRouteConfiguration,
     override val formConfig: VGSCheckoutMultiplexingFormConfiguration
 ) : CheckoutConfiguration() {
 
+    /**
+     * @throws IllegalArgumentException if token is not valid.
+     */
     @JvmOverloads
+    @Throws(IllegalArgumentException::class)
     constructor(
         vaultID: String,
+        token: String,
         environment: String = DEFAULT_ENVIRONMENT
     ) : this(
+        token,
         vaultID,
         environment,
         VGSCheckoutRouteConfiguration(),
