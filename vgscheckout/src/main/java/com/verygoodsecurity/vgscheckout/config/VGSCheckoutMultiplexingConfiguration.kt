4c8c998d1bb0adbe5fed3a458b29728252076a02
DmytroDm <72735363+DmytroDm@users.noreply.github.com>
2021-09-07 12:53:58 +0300

    Add JWT token validation (#33)

--- vgscheckout/src/main/java/com/verygoodsecurity/vgscheckout/config/VGSCheckoutMultiplexingConfiguration.kt
+++ vgscheckout/src/main/java/com/verygoodsecurity/vgscheckout/config/VGSCheckoutMultiplexingConfiguration.kt
@@ -4,25 +4,60 @@ import com.verygoodsecurity.vgscheckout.config.core.CheckoutConfiguration
 import com.verygoodsecurity.vgscheckout.config.core.DEFAULT_ENVIRONMENT
 import com.verygoodsecurity.vgscheckout.config.networking.VGSCheckoutRouteConfiguration
 import com.verygoodsecurity.vgscheckout.config.ui.VGSCheckoutMultiplexingFormConfiguration
+import com.verygoodsecurity.vgscheckout.util.extension.contains
+import com.verygoodsecurity.vgscheckout.util.extension.decodeJwtPayload
+import com.verygoodsecurity.vgscheckout.util.extension.toJson
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
 class VGSCheckoutMultiplexingConfiguration private constructor(
+    internal val token: String,
     override val vaultID: String,
     override val environment: String,
     override val routeConfig: VGSCheckoutRouteConfiguration,
     override val formConfig: VGSCheckoutMultiplexingFormConfiguration
 ) : CheckoutConfiguration() {
 
+    /**
+     * @throws IllegalArgumentException if token is not valid.
+     */
     @JvmOverloads
+    @Throws(IllegalArgumentException::class)
     constructor(
         vaultID: String,
-        environment: String = DEFAULT_ENVIRONMENT,
-        formConfig: VGSCheckoutMultiplexingFormConfiguration = VGSCheckoutMultiplexingFormConfiguration()
+        token: String,
+        environment: String = DEFAULT_ENVIRONMENT
     ) : this(
+        VGSCheckoutMultiplexingCredentialsValidator.validateJWT(vaultID, token),
         vaultID,
         environment,
         VGSCheckoutRouteConfiguration(),
-        formConfig
+        VGSCheckoutMultiplexingFormConfiguration()
     )
+}
+
+internal object VGSCheckoutMultiplexingCredentialsValidator {
+
+    private const val RESTRICTED_TOKEN_ROLE_SCOPE = "transfers:write"
+
+    private const val RESOURCE_ACCESS_KEY = "resource_access"
+    private const val APP_ID_KEY_PREFIX = "multiplexing-app-"
+    private const val ROLES_KEY = "roles"
+
+    @Throws(IllegalArgumentException::class)
+    fun validateJWT(vaultID: String, token: String): String {
+        val payload = token.decodeJwtPayload()?.toJson()
+            ?: throw IllegalArgumentException("Can't parse invalid JWT token.")
+
+        val roles = payload.optJSONObject(RESOURCE_ACCESS_KEY)
+            ?.optJSONObject(APP_ID_KEY_PREFIX + vaultID)
+            ?.optJSONArray(ROLES_KEY)
+            ?: throw IllegalArgumentException("JWT token doesn't contains roles.")
+
+        if (roles.contains(RESTRICTED_TOKEN_ROLE_SCOPE)) {
+            throw IllegalArgumentException("JWT token contains restricted role [$RESTRICTED_TOKEN_ROLE_SCOPE].")
+        }
+
+        return token
+    }
 }
