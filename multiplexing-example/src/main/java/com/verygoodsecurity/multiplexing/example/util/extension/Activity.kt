7b57e387b707a9e0d7a269debcb3457de0a6c96c
Dmytro Kos <dmitriy.kosolapenko@vgs.io>
2021-09-20 17:58:53 +0300

    Create Checkout, Multiplexing examples

---
+++ multiplexing-example/src/main/java/com/verygoodsecurity/multiplexing/example/util/extension/Activity.kt
@@ -0,0 +1,8 @@
+package com.verygoodsecurity.multiplexing.example.util.extension
+
+import android.app.Activity
+import android.widget.Toast
+
+fun Activity.showShort(message: String) {
+    Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+}
