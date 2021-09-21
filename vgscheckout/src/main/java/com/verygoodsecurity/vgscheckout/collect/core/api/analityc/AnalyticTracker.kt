268dafc11c11751619565e2c15a565c4d2e64051
dmytrodmytryshyn <dmytro.dmytryshyn@vgs.io>
2021-09-21 17:33:08 +0300

    Fix tests

--- vgscheckout/src/main/java/com/verygoodsecurity/vgscheckout/collect/core/api/analityc/AnalyticTracker.kt
+++ vgscheckout/src/main/java/com/verygoodsecurity/vgscheckout/collect/core/api/analityc/AnalyticTracker.kt
@@ -3,6 +3,8 @@ package com.verygoodsecurity.vgscheckout.collect.core.api.analityc
 import com.verygoodsecurity.vgscheckout.collect.core.api.analityc.event.core.Event
 
 internal interface AnalyticTracker {
+
     var isEnabled: Boolean
+
     fun log(event: Event)
 }
