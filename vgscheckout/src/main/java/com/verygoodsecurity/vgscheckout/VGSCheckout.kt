bc3f7e91b76967e09c219940febc8e676f6d063a
dmytrodmytryshyn <dmytro.dmytryshyn@vgs.io>
2021-09-28 14:48:07 +0300

    Add overloaded present function implementation which start multiplexing flow

--- vgscheckout/src/main/java/com/verygoodsecurity/vgscheckout/VGSCheckout.kt
+++ vgscheckout/src/main/java/com/verygoodsecurity/vgscheckout/VGSCheckout.kt
@@ -2,7 +2,9 @@ package com.verygoodsecurity.vgscheckout
 
 import androidx.activity.ComponentActivity
 import androidx.activity.result.ActivityResultLauncher
+import com.verygoodsecurity.vgscheckout.config.VGSCheckoutMultiplexingConfiguration
 import com.verygoodsecurity.vgscheckout.config.core.CheckoutConfiguration
+import com.verygoodsecurity.vgscheckout.config.core.DEFAULT_ENVIRONMENT
 import com.verygoodsecurity.vgscheckout.model.CheckoutResultContract
 
 class VGSCheckout internal constructor(
@@ -15,6 +17,33 @@ class VGSCheckout internal constructor(
 
     fun present(config: CheckoutConfiguration) {
         activityResultLauncher.launch(CheckoutResultContract.Args(config))
+    }
+
+    /**
+     * Start checkout with multiplexing configuration.
+     *
+     * @param token client backend access token.
+     * @param vaultID VGS vault id.
+     * @param environment type of vault to communicate with.
+     * @param isAnalyticsEnabled true if checkout should send analytics events, false otherwise.
+     */
+    @JvmOverloads
+    fun present(
+        token: String,
+        vaultID: String,
+        environment: String = DEFAULT_ENVIRONMENT,
+        isAnalyticsEnabled: Boolean = true
+    ) {
+        activityResultLauncher.launch(
+            CheckoutResultContract.Args(
+                VGSCheckoutMultiplexingConfiguration(
+                    token,
+                    vaultID,
+                    environment,
+                    isAnalyticsEnabled
+                )
+            )
+        )
     }
 
     companion object {
