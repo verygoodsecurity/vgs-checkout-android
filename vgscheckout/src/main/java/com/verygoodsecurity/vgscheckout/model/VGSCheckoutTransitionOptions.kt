4e49637fa4b8f060c00d6c33c9cdc71ef332e541
dmytrodmytryshyn <dmytro.dmytryshyn@vgs.io>
2021-09-29 14:24:24 +0300

    Update comments

--- vgscheckout/src/main/java/com/verygoodsecurity/vgscheckout/model/VGSCheckoutTransitionOptions.kt
+++ vgscheckout/src/main/java/com/verygoodsecurity/vgscheckout/model/VGSCheckoutTransitionOptions.kt
@@ -4,7 +4,7 @@ import android.content.Context
 import androidx.core.app.ActivityOptionsCompat
 
 /**
- * Helper class for building custom animation to run when the checkout is displayed.
+ * Helper class for specifying custom animation to run when the checkout is displayed.
  */
 class VGSCheckoutTransitionOptions private constructor(internal val options: ActivityOptionsCompat) {
 
@@ -14,11 +14,11 @@ class VGSCheckoutTransitionOptions private constructor(internal val options: Act
          * Create an VGSCheckoutTransitionOptions specifying a custom animation to run when the
          * checkout is displayed.
          *
-         * @param context Who is defining this. This is the application that the
+         * @param context who is defining this. This is the application that the
          * animation resources will be loaded from.
-         * @param enterAnimId A resource ID of the animation resource to use for the
+         * @param enterAnimId a resource ID of the animation resource to use for the
          * incoming activity. Use 0 for no animation.
-         * @param exitAnimId A resource ID of the animation resource to use for the
+         * @param exitAnimId a resource ID of the animation resource to use for the
          * outgoing activity. Use 0 for no animation.
          */
         @JvmStatic
