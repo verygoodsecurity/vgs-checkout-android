11523d1436d28ff26f57fce80b25cf7b71f1d52d
dmytrodmytryshyn <dmytro.dmytryshyn@vgs.io>
2021-09-29 16:12:41 +0300

    Add scale and reveal transition create functions

--- vgscheckout/src/main/java/com/verygoodsecurity/vgscheckout/model/VGSCheckoutTransitionOptions.kt
+++ vgscheckout/src/main/java/com/verygoodsecurity/vgscheckout/model/VGSCheckoutTransitionOptions.kt
@@ -1,6 +1,9 @@
+@file:Suppress("unused")
+
 package com.verygoodsecurity.vgscheckout.model
 
 import android.content.Context
+import android.view.View
 import androidx.core.app.ActivityOptionsCompat
 
 /**
@@ -20,6 +23,8 @@ class VGSCheckoutTransitionOptions private constructor(internal val options: Act
          * incoming activity. Use 0 for no animation.
          * @param exitAnimId a resource ID of the animation resource to use for the
          * outgoing activity. Use 0 for no animation.
+         * @return [VGSCheckoutTransitionOptions] object that can be used as a parameter of
+         * [com.verygoodsecurity.vgscheckout.VGSCheckout.present] function.
          */
         @JvmStatic
         fun makeCustomAnimation(context: Context, enterAnimId: Int, exitAnimId: Int) =
@@ -30,5 +35,65 @@ class VGSCheckoutTransitionOptions private constructor(internal val options: Act
                     exitAnimId
                 )
             )
+
+        /**
+         * Create an VGSCheckoutTransitionOptions specifying an animation where the checkout
+         * is scaled from a small originating area of the screen to its final full representation.
+         *
+         * @param source the View that the new activity is animating from. This
+         * defines the coordinate space for startX and startY.
+         * @param startX the x starting location of the new activity, relative to source.
+         * @param startY the y starting location of the activity, relative to source.
+         * @param width the initial width of the new activity.
+         * @param height the initial height of the new activity.
+         * @return [VGSCheckoutTransitionOptions] object that can be used as a parameter of
+         * [com.verygoodsecurity.vgscheckout.VGSCheckout.present] function.
+         */
+        @JvmStatic
+        fun makeScaleUpAnimation(
+            source: View,
+            startX: Int,
+            startY: Int,
+            width: Int,
+            height: Int
+        ) = VGSCheckoutTransitionOptions(
+            ActivityOptionsCompat.makeScaleUpAnimation(
+                source,
+                startX,
+                startY,
+                width,
+                height
+            )
+        )
+
+        /**
+         * Create an VGSCheckoutTransitionOptions specifying an animation where the checkout
+         * is revealed from a small originating area of the screen to its final full representation.
+         *
+         * @param source the View that the new activity is animating from. This
+         * defines the coordinate space for startX and startY.
+         * @param startX the x starting location of the new activity, relative to source.
+         * @param startY the y starting location of the activity, relative to source.
+         * @param width the initial width of the new activity.
+         * @param height the initial height of the new activity.
+         * @return [VGSCheckoutTransitionOptions] object that can be used as a parameter of
+         * [com.verygoodsecurity.vgscheckout.VGSCheckout.present] function.
+         */
+        @JvmStatic
+        fun makeClipRevealAnimation(
+            source: View,
+            startX: Int,
+            startY: Int,
+            width: Int,
+            height: Int
+        ) = VGSCheckoutTransitionOptions(
+            ActivityOptionsCompat.makeClipRevealAnimation(
+                source,
+                startX,
+                startY,
+                width,
+                height
+            )
+        )
     }
 }
