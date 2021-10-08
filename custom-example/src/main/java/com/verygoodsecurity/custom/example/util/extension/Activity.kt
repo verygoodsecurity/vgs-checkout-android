e983fb4ee5c66161fbc1cc9986122c46447e06b7
Dmytro Kos <dmitriy.kosolapenko@vgs.io>
2021-10-08 18:11:29 +0300

    rename module (#88)

--- custom-example/src/main/java/com/verygoodsecurity/custom/example/util/extension/Activity.kt
+++ custom-example/src/main/java/com/verygoodsecurity/custom/example/util/extension/Activity.kt
@@ -1,4 +1,4 @@
-package com.verygoodsecurity.checkout.example.util.extension
+package com.verygoodsecurity.custom.example.util.extension
 
 import android.app.Activity
 import android.widget.Toast
--- vgscheckout/src/androidTest/java/com/verygoodsecurity/vgscheckout/custom/ui/DefaultCheckoutSetupTest.kt
+++ vgscheckout/src/androidTest/java/com/verygoodsecurity/vgscheckout/custom/ui/DefaultCheckoutSetupTest.kt
@@ -1,4 +1,4 @@
-package com.verygoodsecurity.vgscheckout.checkout.ui
+package com.verygoodsecurity.vgscheckout.custom.ui
 
 import android.content.Context
 import android.content.Intent
