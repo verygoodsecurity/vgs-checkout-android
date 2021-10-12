0b290b90657de3e74b1efc28ec2647888f1b900d
dmytrodmytryshyn <dmytro.dmytryshyn@vgs.io>
2021-10-12 14:23:32 +0300

    Add VGSCheckoutHostnamePolicy.kt javadoc comments.

--- vgscheckout/src/main/java/com/verygoodsecurity/vgscheckout/config/networking/core/VGSCheckoutHostnamePolicy.kt
+++ vgscheckout/src/main/java/com/verygoodsecurity/vgscheckout/config/networking/core/VGSCheckoutHostnamePolicy.kt
@@ -3,14 +3,36 @@ package com.verygoodsecurity.vgscheckout.config.networking.core
 import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
 
+/**
+ * Defines base url to send data.
+ */
 sealed class VGSCheckoutHostnamePolicy : Parcelable {
 
+    /**
+     * Default vault url.
+     */
     @Parcelize
     object Vault : VGSCheckoutHostnamePolicy()
 
+    /**
+     * Custom url hostname. Should be configured on the dashboard.
+     *
+     * @param hostname custom hostname.
+     */
     @Parcelize
     data class CustomHostname constructor(val hostname: String) : VGSCheckoutHostnamePolicy()
 
+    /**
+     *  Locally configured [VGS-Satellite](https://www.verygoodsecurity.com/docs/vgs-satellite) url.
+     *
+     *  IMPORTANT: VGS-Satellite uses http protocol, starting from android 8.1(27) communication using
+     *  http protocol is disabled by default, developer must add network security config to allow it
+     *  or allow clear text traffic in manifest of app.
+     *  Read more [here](https://developer.android.com/training/articles/security-config#CleartextTrafficPermitted)
+     *
+     *  @param localhost ip.
+     *  @param port integer value from 1 to 65353.
+     */
     @Parcelize
     data class Local constructor(val localhost: String, val port: Int) : VGSCheckoutHostnamePolicy()
 }
