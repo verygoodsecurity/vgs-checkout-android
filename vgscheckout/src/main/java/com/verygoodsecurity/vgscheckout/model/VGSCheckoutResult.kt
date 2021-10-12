c71ef0779faf5f3330fc42b7d0a0f90f7184e941
dmytrodmytryshyn <dmytro.dmytryshyn@vgs.io>
2021-10-12 11:54:52 +0300

    Add VGSCheckoutResult.kt java doc.

--- vgscheckout/src/main/java/com/verygoodsecurity/vgscheckout/model/VGSCheckoutResult.kt
+++ vgscheckout/src/main/java/com/verygoodsecurity/vgscheckout/model/VGSCheckoutResult.kt
@@ -3,26 +3,41 @@ package com.verygoodsecurity.vgscheckout.model
 import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
 
+/**
+ * The result of an attempt to submit checkout form.
+ */
 sealed class VGSCheckoutResult : Parcelable {
 
+    /**
+     * Checkout was successfully completed.
+     *
+     * @param code http response code.
+     * @param body http response body.
+     */
     @Parcelize
     data class Success constructor(val code: Int?, val body: String?) : VGSCheckoutResult() {
-        override fun toString(): String {
-            return "${this.javaClass.simpleName}\ncode: $code \nbody:$body"
-        }
+
+        override fun toString() = "${this.javaClass.simpleName}\ncode: $code \nbody:$body"
     }
 
+    /**
+     * Checkout was failed due network errors or invalid setup.
+     *
+     * @param code http response code or local error code [com.verygoodsecurity.vgscheckout.collect.core.model.network.VGSError].
+     * @param body http response body or local error message [com.verygoodsecurity.vgscheckout.collect.core.model.network.VGSError].
+     */
     @Parcelize
     data class Failed constructor(val code: Int?, val body: String?) : VGSCheckoutResult() {
-        override fun toString(): String {
-            return "${this.javaClass.simpleName}\ncode: $code \nbody:$body"
-        }
+
+        override fun toString() = "${this.javaClass.simpleName}\ncode: $code \nbody:$body"
     }
 
+    /**
+     * Checkout cancelled by user.
+     */
     @Parcelize
     object Canceled : VGSCheckoutResult() {
-        override fun toString(): String {
-            return this.javaClass.simpleName
-        }
+
+        override fun toString(): String = this.javaClass.simpleName
     }
 }
