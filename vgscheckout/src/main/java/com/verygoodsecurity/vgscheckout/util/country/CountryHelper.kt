b1d640bc35e0281e89e79d4d20ef5a29a0aac59f
dmytrodmytryshyn <dmytro.dmytryshyn@vgs.io>
2021-10-13 16:33:49 +0300

    Add all possible countries, handle postal code visibility, city ime options on country change

--- vgscheckout/src/main/java/com/verygoodsecurity/vgscheckout/util/country/CountryHelper.kt
+++ vgscheckout/src/main/java/com/verygoodsecurity/vgscheckout/util/country/CountryHelper.kt
@@ -2,61 +2,104 @@ package com.verygoodsecurity.vgscheckout.util.country
 
 import com.verygoodsecurity.vgscheckout.util.country.model.Country
 import com.verygoodsecurity.vgscheckout.util.country.model.PostalAddressType
-import com.verygoodsecurity.vgscheckout.util.country.model.RegionType
 import java.util.*
+
+private const val EMPTY = ""
 
 internal object CountriesHelper {
 
-    val countries: Array<Country> by lazy { generateCountriesList() }
+    val countries: List<Country> by lazy { Locale.getISOCountries().map { getCountry(it) } }
 
-    private const val EMPTY = ""
-
-    private fun generateCountriesList(): Array<Country> {
-        return arrayOf(
-            getCountry(ISO.USA),
-            getCountry(ISO.CANADA),
-            getCountry(ISO.AUSTRALIA),
-            getCountry(ISO.NEW_ZEALAND),
-            getCountry(ISO.UNITED_KINGDOM)
-        )
-    }
-
-    private fun getCountry(iso: String) = Country(
+    fun getCountry(iso: String) = Country(
         Locale(EMPTY, iso).getDisplayCountry(Locale.US),
         iso,
-        getRegionType(iso),
         getPostalAddressType(iso),
         getPostalAddressRegex(iso)
     )
 
-    private fun getPostalAddressType(iso: String) = when (iso) {
-        ISO.USA -> PostalAddressType.ZIP
+    private fun getPostalAddressType(iso: String) = when {
+        iso == ISO.USA -> PostalAddressType.ZIP
+        ISO.COUNTRIES_ISO_WITHOUT_POSTAL_ADDRESS.contains(iso) -> PostalAddressType.NOTHING
         else -> PostalAddressType.POSTAL
-    }
-
-    private fun getRegionType(iso: String) = when (iso) {
-        ISO.USA, ISO.AUSTRALIA -> RegionType.STATE
-        ISO.CANADA -> RegionType.PROVINCE
-        ISO.NEW_ZEALAND -> RegionType.SUBURB
-        ISO.UNITED_KINGDOM -> RegionType.COUNTY
-        else -> RegionType.UNKNOWN
     }
 
     private fun getPostalAddressRegex(iso: String) = when (iso) {
         ISO.USA -> "^([0-9]{5})(?:-([0-9]{4}))?\$"
-        ISO.CANADA -> "^([ABCEGHJKLMNPRSTVXY][0-9][ABCEGHJKLMNPRSTVWXYZ])\\s*([0-9][ABCEGHJKLMNPRSTVWXYZ][0-9])\$"
-        ISO.AUSTRALIA -> "^\\d{4}\$"
-        ISO.NEW_ZEALAND -> "^\\d{4}\$"
-        ISO.UNITED_KINGDOM -> ".+"
         else -> ".+"
     }
 
     object ISO {
 
         internal const val USA = "US"
-        internal const val CANADA = "CA"
-        internal const val AUSTRALIA = "AU"
-        internal const val NEW_ZEALAND = "NZ"
-        internal const val UNITED_KINGDOM = "GB"
+
+        internal val COUNTRIES_ISO_WITHOUT_POSTAL_ADDRESS = arrayOf(
+            "AE",
+            "AG",
+            "AN",
+            "AO",
+            "AW",
+            "BF",
+            "BI",
+            "BJ",
+            "BO",
+            "BS",
+            "BW",
+            "BZ",
+            "CD",
+            "CF",
+            "CG",
+            "CI",
+            "CK",
+            "CM",
+            "DJ",
+            "DM",
+            "ER",
+            "FJ",
+            "GD",
+            "GH",
+            "GM",
+            "GN",
+            "GQ",
+            "GY",
+            "HK",
+            "IE",
+            "JM",
+            "KE",
+            "KI",
+            "KM",
+            "KN",
+            "KP",
+            "LC",
+            "ML",
+            "MO",
+            "MR",
+            "MS",
+            "MU",
+            "MW",
+            "NR",
+            "NU",
+            "PA",
+            "QA",
+            "RW",
+            "SB",
+            "SC",
+            "SL",
+            "SO",
+            "SR",
+            "ST",
+            "SY",
+            "TF",
+            "TK",
+            "TL",
+            "TO",
+            "TT",
+            "TV",
+            "TZ",
+            "UG",
+            "VU",
+            "YE",
+            "ZA",
+            "ZW",
+        )
     }
 }
