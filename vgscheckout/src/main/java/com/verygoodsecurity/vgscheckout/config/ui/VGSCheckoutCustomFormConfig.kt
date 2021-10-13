38afd475902fee6437bb2f6cb338c2c2ed753c22
dmytrodmytryshyn <dmytro.dmytryshyn@vgs.io>
2021-10-13 12:58:38 +0300

    Add ViewOptions.kt, CheckoutFormConfig.kt and all it childs javadoc.

--- vgscheckout/src/main/java/com/verygoodsecurity/vgscheckout/config/ui/VGSCheckoutCustomFormConfig.kt
+++ vgscheckout/src/main/java/com/verygoodsecurity/vgscheckout/config/ui/VGSCheckoutCustomFormConfig.kt
@@ -5,6 +5,12 @@ import com.verygoodsecurity.vgscheckout.config.ui.view.address.VGSCheckoutCustom
 import com.verygoodsecurity.vgscheckout.config.ui.view.card.VGSCheckoutCustomCardOptions
 import kotlinx.parcelize.Parcelize
 
+/**
+ * Custom flow checkout form configuration.
+ *
+ * @param cardOptions credit card details block UI options.
+ * @param addressOptions address details block UI options.
+ */
 @Parcelize
 class VGSCheckoutCustomFormConfig @JvmOverloads constructor(
     override val cardOptions: VGSCheckoutCustomCardOptions = VGSCheckoutCustomCardOptions(),
--- vgscheckout/src/main/java/com/verygoodsecurity/vgscheckout/config/ui/VGSCheckoutMultiplexingFormConfig.kt
+++ vgscheckout/src/main/java/com/verygoodsecurity/vgscheckout/config/ui/VGSCheckoutMultiplexingFormConfig.kt
@@ -5,12 +5,23 @@ import com.verygoodsecurity.vgscheckout.config.ui.view.address.VGSCheckoutMultip
 import com.verygoodsecurity.vgscheckout.config.ui.view.card.VGSCheckoutMultiplexingCardOptions
 import kotlinx.parcelize.Parcelize
 
+/**
+ * Multiplexing flow checkout form configuration.
+ *
+ * @param cardOptions credit card details block UI options.
+ * @param addressOptions address details block UI options.
+ */
 @Parcelize
 class VGSCheckoutMultiplexingFormConfig private constructor(
     override val cardOptions: VGSCheckoutMultiplexingCardOptions,
     override val addressOptions: VGSCheckoutMultiplexingBillingAddressOptions
 ) : CheckoutFormConfig() {
 
+    /**
+     * Public constructor.
+     *
+     * @param addressOptions address details block UI options.
+     */
     @JvmOverloads
     constructor(
         addressOptions: VGSCheckoutMultiplexingBillingAddressOptions = VGSCheckoutMultiplexingBillingAddressOptions()
