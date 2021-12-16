1b6bcb63c6530cc23c496e618589cb8d5236810e
dmytrodmytryshyn <dmytro.dmytryshyn@vgs.io>
2021-12-16 13:34:39 +0200

    Important TODO added

--- vgscheckout/src/main/java/com/verygoodsecurity/vgscheckout/VGSCheckoutCallback.kt
+++ vgscheckout/src/main/java/com/verygoodsecurity/vgscheckout/VGSCheckoutCallback.kt
@@ -2,6 +2,8 @@ package com.verygoodsecurity.vgscheckout
 
 import com.verygoodsecurity.vgscheckout.model.VGSCheckoutResult
 
+// TODO: Add SAM conversions to VGSCheckoutCallback if it should not has more than one function.
+
 /**
  * Checkout form callback interface definition.
  */
