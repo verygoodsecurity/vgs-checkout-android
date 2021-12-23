9f68563687de48c1f440ee9b83d71e393b90ccec
dmytrodmytryshyn <dmytro.dmytryshyn@vgs.io>
2021-12-23 11:07:06 +0200

    Add comments for VGSCheckoutConfigInitCallback.kt

--- vgscheckout/src/main/java/com/verygoodsecurity/vgscheckout/VGSCheckoutCallback.kt
+++ vgscheckout/src/main/java/com/verygoodsecurity/vgscheckout/VGSCheckoutCallback.kt
@@ -2,8 +2,6 @@ package com.verygoodsecurity.vgscheckout
 
 import com.verygoodsecurity.vgscheckout.model.VGSCheckoutResult
 
-// TODO: Add SAM conversions to VGSCheckoutCallback if it should not has more than one function.
-
 /**
  * Checkout form callback interface definition.
  */
--- vgscheckout/src/main/java/com/verygoodsecurity/vgscheckout/VGSCheckoutConfigInitCallback.kt
+++ vgscheckout/src/main/java/com/verygoodsecurity/vgscheckout/VGSCheckoutConfigInitCallback.kt
@@ -4,11 +4,21 @@ import com.verygoodsecurity.vgscheckout.config.core.CheckoutConfig
 import com.verygoodsecurity.vgscheckout.exception.VGSCheckoutException
 
 /**
- * TODO: Add comments
+ * Checkout config creating callback interface definition.
  */
 interface VGSCheckoutConfigInitCallback<T : CheckoutConfig> {
 
+    /**
+     * Invoked when checkout config creation successfully complete.
+     *
+     * @param config instance of checkout config.
+     */
     fun onSuccess(config: T)
 
+    /**
+     * Invoked when checkout config creation failed.
+     *
+     * @param exception explains why checkout config creation failed.
+     */
     fun onFailure(exception: VGSCheckoutException)
 }
