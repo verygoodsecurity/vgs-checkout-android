a5544e1e2365236a97443285b7079aa52b0a4c47
Dmytro <dmitriy.kosolapenko@vgs.io>
2022-02-01 14:16:31 +0200

    remove samples

--- payment-example/src/main/java/com/verygoodsecurity/payments/data/source/remote/payment/dto/CreateTransactionResponse.kt
+++
@@ -1,8 +0,0 @@
-package com.verygoodsecurity.payments.data.source.remote.payment.dto
-
-import com.google.gson.annotations.SerializedName
-import com.verygoodsecurity.payments.data.entity.Transaction
-
-data class CreateTransactionResponse constructor(
-    @SerializedName("data") val transaction: Transaction
-)
