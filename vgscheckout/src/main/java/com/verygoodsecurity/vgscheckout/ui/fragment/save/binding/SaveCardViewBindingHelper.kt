7c0314f31bf966564622eee33cfe61b575f8ffaf
dmytrodmytryshyn <dmytro.dmytryshyn@vgs.io>
2022-04-11 16:43:10 +0300

    Remove unused internal storage object

--- vgscheckout/src/main/java/com/verygoodsecurity/vgscheckout/ui/fragment/save/binding/SaveCardViewBindingHelper.kt
+++ vgscheckout/src/main/java/com/verygoodsecurity/vgscheckout/ui/fragment/save/binding/SaveCardViewBindingHelper.kt
@@ -9,8 +9,6 @@ import com.google.android.material.button.MaterialButton
 import com.google.android.material.checkbox.MaterialCheckBox
 import com.google.android.material.textview.MaterialTextView
 import com.verygoodsecurity.vgscheckout.R
-import com.verygoodsecurity.vgscheckout.collect.core.storage.InternalStorage
-import com.verygoodsecurity.vgscheckout.collect.view.InputFieldView
 import com.verygoodsecurity.vgscheckout.collect.widget.*
 
 // TODO: Remove all unused views
@@ -19,43 +17,32 @@ internal class SaveCardViewBindingHelper(inflater: LayoutInflater, @LayoutRes la
 
     val rootView: View = inflater.inflate(layoutId, null)
 
-    val cardDetailsMtv: MaterialTextView by lazy { findViewById(R.id.mtvCardDetailsTitle) }
-    val cardDetailsLL: LinearLayoutCompat by lazy { findViewById(R.id.llCardDetails) }
-    val cardHolderTil: VGSTextInputLayout by lazy { findViewById(R.id.vgsTilCardHolder) }
-    val cardHolderEt: PersonNameEditText by lazy { findViewById(R.id.vgsEtCardHolder) }
-    val cardNumberTil: VGSTextInputLayout by lazy { findViewById(R.id.vgsTilCardNumber) }
-    val cardNumberEt: VGSCardNumberEditText by lazy { findViewById(R.id.vgsEtCardNumber) }
-    val expirationDateTil: VGSTextInputLayout by lazy { findViewById(R.id.vgsTilExpirationDate) }
-    val expirationDateEt: ExpirationDateEditText by lazy { findViewById(R.id.vgsEtExpirationDate) }
-    val securityCodeTil: VGSTextInputLayout by lazy { findViewById(R.id.vgsTilSecurityCode) }
-    val securityCodeEt: CardVerificationCodeEditText by lazy { findViewById(R.id.vgsEtSecurityCode) }
+    val cardDetailsMtv: MaterialTextView by lazy { rootView.findViewById(R.id.mtvCardDetailsTitle) }
+    val cardDetailsLL: LinearLayoutCompat by lazy { rootView.findViewById(R.id.llCardDetails) }
+    val cardHolderTil: VGSTextInputLayout by lazy { rootView.findViewById(R.id.vgsTilCardHolder) }
+    val cardHolderEt: PersonNameEditText by lazy { rootView.findViewById(R.id.vgsEtCardHolder) }
+    val cardNumberTil: VGSTextInputLayout by lazy { rootView.findViewById(R.id.vgsTilCardNumber) }
+    val cardNumberEt: VGSCardNumberEditText by lazy { rootView.findViewById(R.id.vgsEtCardNumber) }
+    val expirationDateTil: VGSTextInputLayout by lazy { rootView.findViewById(R.id.vgsTilExpirationDate) }
+    val expirationDateEt: ExpirationDateEditText by lazy { rootView.findViewById(R.id.vgsEtExpirationDate) }
+    val securityCodeTil: VGSTextInputLayout by lazy { rootView.findViewById(R.id.vgsTilSecurityCode) }
+    val securityCodeEt: CardVerificationCodeEditText by lazy { rootView.findViewById(R.id.vgsEtSecurityCode) }
 
-    val billingAddressMtv: MaterialTextView by lazy { findViewById(R.id.mtvBillingAddressTitle) }
-    val billingAddressLL: LinearLayoutCompat by lazy { findViewById(R.id.llBillingAddress) }
-    val countryTil: VGSTextInputLayout by lazy { findViewById(R.id.vgsTilCountry) }
-    val countryEt: VGSCountryEditText by lazy { findViewById(R.id.vgsEtCountry) }
-    val addressTil: VGSTextInputLayout by lazy { findViewById(R.id.vgsTilAddress) }
-    val addressEt: VGSEditText by lazy { findViewById(R.id.vgsEtAddress) }
-    val optionalAddressTil: VGSTextInputLayout by lazy { findViewById(R.id.vgsTilAddressOptional) }
-    val optionalAddressEt: VGSEditText by lazy { findViewById(R.id.vgsEtAddressOptional) }
-    val cityTil: VGSTextInputLayout by lazy { findViewById(R.id.vgsTilCity) }
-    val cityEt: VGSEditText by lazy { findViewById(R.id.vgsEtCity) }
-    val postalCodeTil: VGSTextInputLayout by lazy { findViewById(R.id.vgsTilPostalCode) }
-    val postalCodeEt: VGSEditText by lazy { findViewById(R.id.vgsEtPostalCode) }
+    val billingAddressMtv: MaterialTextView by lazy { rootView.findViewById(R.id.mtvBillingAddressTitle) }
+    val billingAddressLL: LinearLayoutCompat by lazy { rootView.findViewById(R.id.llBillingAddress) }
+    val countryTil: VGSTextInputLayout by lazy { rootView.findViewById(R.id.vgsTilCountry) }
+    val countryEt: VGSCountryEditText by lazy { rootView.findViewById(R.id.vgsEtCountry) }
+    val addressTil: VGSTextInputLayout by lazy { rootView.findViewById(R.id.vgsTilAddress) }
+    val addressEt: VGSEditText by lazy { rootView.findViewById(R.id.vgsEtAddress) }
+    val optionalAddressTil: VGSTextInputLayout by lazy { rootView.findViewById(R.id.vgsTilAddressOptional) }
+    val optionalAddressEt: VGSEditText by lazy { rootView.findViewById(R.id.vgsEtAddressOptional) }
+    val cityTil: VGSTextInputLayout by lazy { rootView.findViewById(R.id.vgsTilCity) }
+    val cityEt: VGSEditText by lazy { rootView.findViewById(R.id.vgsEtCity) }
+    val postalCodeTil: VGSTextInputLayout by lazy { rootView.findViewById(R.id.vgsTilPostalCode) }
+    val postalCodeEt: VGSEditText by lazy { rootView.findViewById(R.id.vgsEtPostalCode) }
 
-    val cityPostalAddressSpace: Space by lazy { findViewById(R.id.cityPostalAddressSpace) }
+    val cityPostalAddressSpace: Space by lazy { rootView.findViewById(R.id.cityPostalAddressSpace) }
 
-    val saveCardCheckbox: MaterialCheckBox by lazy { findViewById(R.id.mcbSaveCard) }
-    val saveCardButton: MaterialButton by lazy { findViewById(R.id.mbSaveCard) }
-
-    private val storage = InternalStorage()
-
-    fun getAssociatedList() = storage.getAssociatedList()
-
-    private fun <T : View> findViewById(id: Int): T {
-        return rootView.findViewById<T>(id).also {
-            storage.performSubscription(it as? InputFieldView)
-        }
-    }
-
+    val saveCardCheckbox: MaterialCheckBox by lazy { rootView.findViewById(R.id.mcbSaveCard) }
+    val saveCardButton: MaterialButton by lazy { rootView.findViewById(R.id.mbSaveCard) }
 }
