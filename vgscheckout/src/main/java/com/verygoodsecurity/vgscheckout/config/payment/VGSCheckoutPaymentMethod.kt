351209dad0b8e5874a5ec65806c4041a72e69468
dmytrodmytryshyn <dmytro.dmytryshyn@vgs.io>
2022-04-20 12:16:52 +0300

    Rename ids -> cardIds

--- vgscheckout/src/main/java/com/verygoodsecurity/vgscheckout/config/payment/VGSCheckoutPaymentMethod.kt
+++ vgscheckout/src/main/java/com/verygoodsecurity/vgscheckout/config/payment/VGSCheckoutPaymentMethod.kt
@@ -1,7 +1,7 @@
 package com.verygoodsecurity.vgscheckout.config.payment
 
 import androidx.annotation.Size
-import com.verygoodsecurity.vgscheckout.config.payment.VGSCheckoutPaymentMethod.SavedCards.Companion.MAX_IDS_SIZE
+import com.verygoodsecurity.vgscheckout.config.payment.VGSCheckoutPaymentMethod.SavedCards.Companion.MAX_CARDS_SIZE
 import com.verygoodsecurity.vgscheckout.util.logger.VGSCheckoutLogger
 
 /**
@@ -17,24 +17,24 @@ sealed class VGSCheckoutPaymentMethod {
     /**
      * Add ability to use previously saved cards or new card.
      *
-     * @param ids list of cards(financial instruments) ids. Max length [MAX_IDS_SIZE].
+     * @param cardIds list of cards(financial instruments) ids. Max length [MAX_CARDS_SIZE].
      */
     data class SavedCards constructor(
-        @Size(max = MAX_IDS_SIZE) private val ids: List<String>
+        @Size(max = MAX_CARDS_SIZE) private val cardIds: List<String>
     ) : VGSCheckoutPaymentMethod() {
 
         fun getIds(): List<String> {
-            return if (ids.size > MAX_IDS_SIZE) {
-                VGSCheckoutLogger.warn(message = "Max saved cards limit to fetch is $MAX_IDS_SIZE! Current saved cards count is: ${ids.size})!")
-                ids.subList(0, MAX_IDS_SIZE.toInt())
+            return if (cardIds.size > MAX_CARDS_SIZE) {
+                VGSCheckoutLogger.warn(message = "Max saved cards limit to fetch is $MAX_CARDS_SIZE! Current saved cards count is: ${cardIds.size})!")
+                cardIds.subList(0, MAX_CARDS_SIZE.toInt())
             } else {
-                ids
+                cardIds
             }
         }
 
         companion object {
 
-            private const val MAX_IDS_SIZE = 10L
+            private const val MAX_CARDS_SIZE = 10L
         }
     }
 }
