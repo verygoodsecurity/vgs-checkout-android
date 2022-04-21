969945e7ceb8d9500f7dc22e8138756d123d40db
Dmytro <dmitriy.kosolapenko@vgs.io>
2022-04-21 15:31:13 +0300

    add present method for fast start

--- vgscheckout/src/main/java/com/verygoodsecurity/vgscheckout/VGSCheckout.kt
+++ vgscheckout/src/main/java/com/verygoodsecurity/vgscheckout/VGSCheckout.kt
@@ -3,8 +3,10 @@ package com.verygoodsecurity.vgscheckout
 import androidx.activity.result.ActivityResultLauncher
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import com.verygoodsecurity.vgscheckout.config.VGSCheckoutAddCardConfig
 import com.verygoodsecurity.vgscheckout.config.core.CheckoutConfig
 import com.verygoodsecurity.vgscheckout.model.CheckoutResultContract
+import com.verygoodsecurity.vgscheckout.model.VGSCheckoutEnvironment
 import com.verygoodsecurity.vgscheckout.model.VGSCheckoutTransitionOptions
 
 /**
@@ -35,6 +37,33 @@ class VGSCheckout internal constructor(
     constructor(fragment: Fragment, callback: VGSCheckoutCallback? = null) : this(
         registerActivityLauncher(fragment, callback)
     )
+
+    /**
+     * Start checkout with payment instruments configuration.
+     *
+     * @param token client backend access token.
+     * @param vaultID VGS vault id.
+     * @param environment type of vault to communicate with.
+     * @param transitionOptions specifying a custom animation to run when the checkout is displayed.
+     * @param isAnalyticsEnabled true if checkout should send analytics events, false otherwise.
+     */
+    fun present(
+        token: String,
+        vaultID: String,
+        environment: VGSCheckoutEnvironment = VGSCheckoutEnvironment.Sandbox(),
+        transitionOptions: VGSCheckoutTransitionOptions? = null,
+        isAnalyticsEnabled: Boolean = true
+    ) {
+        present(
+            VGSCheckoutAddCardConfig(
+                token,
+                vaultID,
+                environment,
+                isAnalyticsEnabled = isAnalyticsEnabled
+            ),
+            transitionOptions
+        )
+    }
 
     /**
      * Start checkout.
