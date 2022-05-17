a1bb491f9863385988c9158fd12ac5a40a5d96ac
dmytrodmytryshyn <dmytro.dmytryshyn@vgs.io>
2022-05-17 13:37:05 +0300

    Refactor payment example

--- demo/src/main/java/com/verygoodsecurity/vgscheckout/demo/add/PaymentCheckoutActivity.kt
+++ demo/src/main/java/com/verygoodsecurity/vgscheckout/demo/add/PaymentCheckoutActivity.kt
@@ -4,74 +4,96 @@ import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.button.MaterialButton
-import com.verygoodsecurity.vgscheckout.demo.BuildConfig.STORAGE_ID
-import com.verygoodsecurity.vgscheckout.demo.R
-import com.verygoodsecurity.vgscheckout.demo.add.network.HttpClient
+import com.google.gson.Gson
+import com.google.gson.annotations.SerializedName
 import com.verygoodsecurity.vgscheckout.VGSCheckout
 import com.verygoodsecurity.vgscheckout.VGSCheckoutCallback
 import com.verygoodsecurity.vgscheckout.config.VGSCheckoutAddCardConfig
+import com.verygoodsecurity.vgscheckout.demo.BuildConfig
+import com.verygoodsecurity.vgscheckout.demo.R
 import com.verygoodsecurity.vgscheckout.model.VGSCheckoutResult
 import com.verygoodsecurity.vgscheckout.model.VGSCheckoutResultBundle
 import com.verygoodsecurity.vgscheckout.model.response.VGSCheckoutCardResponse
+import okhttp3.*
+import okhttp3.internal.EMPTY_REQUEST
+import java.io.IOException
 
 class PaymentCheckoutActivity : AppCompatActivity(), VGSCheckoutCallback {
 
-    private val vaultId: String = STORAGE_ID
-
-    private val accessClient: HttpClient by lazy { HttpClient() }
+    private val tokenManager: TokenManager = TokenManager()
 
     private lateinit var checkout: VGSCheckout
-
-    private var config: VGSCheckoutAddCardConfig? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_payment_checkout)
-
-        refreshToken()
-
         checkout = VGSCheckout(this, this)
-
-        findViewById<MaterialButton>(R.id.mbPresent).setOnClickListener {
-            config?.let { checkout.present(it) }
-        }
+        findViewById<MaterialButton>(R.id.mbPresent).setOnClickListener { presentCheckout() }
     }
-
 
     override fun onCheckoutResult(result: VGSCheckoutResult) {
-        if (result is VGSCheckoutResult.Canceled) {
-            refreshToken()
-        } else {
-            printResults(result)
+        // Handle checkout result
+        Log.d(
+            this::class.simpleName, """
+            ${result::class.java.simpleName}
+            ${result.data.getParcelable<VGSCheckoutCardResponse>(VGSCheckoutResultBundle.ADD_CARD_RESPONSE)}
+        """.trimIndent()
+        )
+    }
+
+    private fun presentCheckout() {
+        tokenManager.get {
+            if (it?.value == null) {
+                Log.d(this::class.simpleName, "Token is null.")
+                return@get
+            }
+            Log.d(this::class.simpleName, "Token = ${it.value}.")
+            checkout.present(createConfig(it.value))
         }
     }
 
-    private fun printResults(result: VGSCheckoutResult) {
-        when (result) {
-            is VGSCheckoutResult.Success ->
-                result.data.getParcelable<VGSCheckoutCardResponse>(
-                    VGSCheckoutResultBundle.Keys.ADD_CARD_RESPONSE
-                ).let { Log.e(TAG, "Success \n ${it?.body}") }
-            is VGSCheckoutResult.Failed ->
-                result.data.getParcelable<VGSCheckoutCardResponse>(
-                    VGSCheckoutResultBundle.Keys.ADD_CARD_RESPONSE
-                ).let { Log.e(TAG, "Failed \n ${it?.body}") }
-            is VGSCheckoutResult.Canceled -> Log.e(TAG, "Canceled")
+    private fun createConfig(token: String) =
+        VGSCheckoutAddCardConfig(token, BuildConfig.STORAGE_ID)
+}
+
+class TokenManager {
+
+    private val client: OkHttpClient = OkHttpClient().newBuilder().build()
+
+    private val tokenRequest: Request by lazy {
+        Request.Builder()
+            .url("https://multiplexing-demo.verygoodsecurity.io/get-auth-token")
+            .post(EMPTY_REQUEST)
+            .build()
+    }
+
+    fun get(onResult: ((token: Token?) -> Unit)?) {
+        try {
+            client.newCall(tokenRequest).enqueue(object : Callback {
+
+                override fun onFailure(call: Call, e: IOException) {
+                    onResult?.invoke(null)
+                }
+
+                override fun onResponse(call: Call, response: Response) {
+                    onResult?.invoke(parserResponse(response))
+                }
+            })
+        } catch (e: Exception) {
+            onResult?.invoke(null)
         }
     }
 
-    private fun refreshToken() {
-        accessClient.getAccessToken { token ->
-            config = token?.run {
-                VGSCheckoutAddCardConfig(
-                    token,
-                    vaultId
-                )
+    private fun parserResponse(response: Response): Token? {
+        return with(response.body?.string()) {
+            if (this == null) {
+                null
+            } else {
+                Gson().fromJson(this, Token::class.java)
             }
         }
     }
 
-    companion object {
-        private const val TAG = "VGS Checkout Result"
-    }
+    @JvmInline
+    value class Token constructor(@SerializedName("access_token") val value: String)
 }
