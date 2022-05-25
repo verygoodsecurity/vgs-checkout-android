9cbbe85b2e523d35e9efa9a4bdf1391e9c4e89b1
dmytrodmytryshyn <dmytro.dmytryshyn@vgs.io>
2022-05-25 12:52:42 +0300

    Fix VGSCheckoutTextInputLayout child view add

--- vgscheckout/src/main/java/com/verygoodsecurity/vgscheckout/VGSCheckoutTextInputLayout.kt
+++ vgscheckout/src/main/java/com/verygoodsecurity/vgscheckout/VGSCheckoutTextInputLayout.kt
@@ -2,7 +2,6 @@ package com.verygoodsecurity.vgscheckout
 
 import android.content.Context
 import android.util.AttributeSet
-import android.util.Log
 import android.view.View
 import android.view.ViewGroup
 import android.widget.FrameLayout
@@ -16,33 +15,36 @@ class VGSCheckoutTextInputLayout @JvmOverloads constructor(
 ) : TextInputLayout(context, attrs, defStyleAttr) {
 
     override fun addView(child: View?) {
-        Log.d("Test", "addView(child: View?)")
         super.addView(handleAddView(child))
     }
 
     override fun addView(child: View?, index: Int) {
-        Log.d("Test", "addView(child: View?, index: Int)")
         super.addView(handleAddView(child), index)
     }
 
     override fun addView(child: View?, params: ViewGroup.LayoutParams?) {
-        Log.d("Test", "addView(child: View?, params: ViewGroup.LayoutParams?)")
         super.addView(handleAddView(child), params)
     }
 
     override fun addView(child: View?, width: Int, height: Int) {
-        Log.d("Test", "addView(child: View?, width: Int, height: Int)")
         super.addView(handleAddView(child), width, height)
     }
 
     override fun addView(child: View, index: Int, params: ViewGroup.LayoutParams) {
-        Log.d("Test", "addView(child: View, index: Int, params: ViewGroup.LayoutParams)")
         super.addView(handleAddView(child) ?: child, index, params)
     }
 
     private fun handleAddView(child: View?): View? {
         return when (child) {
             is InputFieldView -> {
+                attachViewToParent(
+                    child,
+                    childCount,
+                    LayoutParams(
+                        FrameLayout.LayoutParams.MATCH_PARENT,
+                        FrameLayout.LayoutParams.MATCH_PARENT
+                    )
+                )
                 child.statePreparer.getView()
             }
             is FrameLayout -> child
