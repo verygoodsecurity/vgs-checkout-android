10e59dc088a6d2a405ab1aa34dd3084fbe1966af
Dmytro <dmitriy.kosolapenko@vgs.io>
2022-06-08 17:00:36 +0300

    add composite command

---
+++ vgscheckout/src/main/java/com/verygoodsecurity/vgscheckout/networking/command/core/CompositeCommand.kt
@@ -0,0 +1,53 @@
+package com.verygoodsecurity.vgscheckout.networking.command.core
+
+import android.content.Context
+import com.verygoodsecurity.vgscheckout.exception.VGSCheckoutException
+import com.verygoodsecurity.vgscheckout.networking.command.GetOrderDetails
+import com.verygoodsecurity.vgscheckout.networking.command.GetSavedCardsCommand
+
+internal class CompositeCommand(
+    context: Context,
+    private val commands: Collection<Command<*, *>>
+) : Command<CompositeCommand.Params, CompositeCommand.Result>(context) {
+
+    private var processedCommandsCounter = 0
+
+    fun execute(onResult: (Result) -> Unit) {
+        execute(Params(commands), onResult)
+    }
+
+    override fun run(params: Params, onResult: (Result) -> Unit) {
+        params.commands.forEach { command ->
+            when (command) {
+                is GetSavedCardsCommand -> command.execute {
+                    processedCommandsCounter++
+                    onResult(Result(it, isProcessing()))
+                }
+                is GetOrderDetails -> command.execute {
+                    processedCommandsCounter++
+                    onResult(Result(it, isProcessing()))
+                }
+            }
+        }
+    }
+
+    override fun map(params: Params, exception: VGSCheckoutException): Result = Result()
+
+    private fun isProcessing(): Boolean {
+        return commands.size != processedCommandsCounter
+    }
+
+    override fun cancel() {
+        commands.forEach { it.cancel() }
+    }
+
+    internal data class Params(
+        val commands: Collection<Command<*, *>>
+    ) : Command.Params()
+
+    internal data class Result(
+        val intermediateResult: Command.Result? = null,
+        val isProcessing: Boolean = false
+    ) : Command.Result()
+
+}
