6754e586a7aea0807a3c73a276d847fd55614a55
Dmytro <dmitriy.kosolapenko@vgs.io>
2022-07-22 19:38:31 +0300

    check tests

--- vgscheckout/src/androidTest/java/com/verygoodsecurity/vgscheckout/payments/saved/integration/SavedCardActivityResultTest.kt
+++ vgscheckout/src/androidTest/java/com/verygoodsecurity/vgscheckout/payments/saved/integration/SavedCardActivityResultTest.kt
@@ -29,7 +29,6 @@ import org.junit.runner.RunWith
 import java.util.concurrent.CountDownLatch
 
 @Suppress("SameParameterValue")
-@RunWith(AndroidJUnit4::class)
 class SavedCardActivityResultTest {
 
     private val context: Context = ApplicationProvider.getApplicationContext()
