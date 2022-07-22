4f6f41bbec890217a0f237c654663a13061fdc94
Dmytro <dmitriy.kosolapenko@vgs.io>
2022-07-22 18:36:06 +0300

    check tests

--- vgscheckout/src/androidTest/java/com/verygoodsecurity/vgscheckout/util/extension/VGSCheckoutCardResponse.kt
+++ vgscheckout/src/androidTest/java/com/verygoodsecurity/vgscheckout/util/extension/VGSCheckoutCardResponse.kt
@@ -1,17 +1,16 @@
 package com.verygoodsecurity.vgscheckout.util.extension
 
-import com.google.gson.JsonParser
-import com.google.gson.stream.JsonReader
+import com.google.gson.Gson
 import com.verygoodsecurity.vgscheckout.model.response.VGSCheckoutCardResponse
-import java.io.StringReader
 
 internal fun VGSCheckoutCardResponse.getId(): String {
-    return JsonParser
-        .parseReader(JsonReader(StringReader(body?.trim())).also { it.isLenient = true })
-        .asJsonObject
-        .run {
-            takeIf { it.has("data") }?.run { get("data").asJsonObject }
-                ?.takeIf { it.has("id") }?.run { get("id").asString }
-                ?: ""
-        }
+    return Gson().fromJson(body, CardResponse::class.java).data.id
 }
+
+private data class CardResponse(
+    val data: CardDataResponse
+)
+
+private data class CardDataResponse(
+    val id: String
+)
